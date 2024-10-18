7988f5572fe5b7d705cc44efbd281442d3ac8078
andreeamillan <anlmilla@ucsc.edu>
2024-10-17 17:59:50 -0700

    pseudocode comments

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - The purpose of this lab is to psuedocode a simple computer game.
+// Author: Andreea Millan
+// Date: October 17, 2024
 
 // Constants
 
@@ -19,3 +19,14 @@ function main() {
 
 // let's get this party started
 main();
+// Assign a player input box, which they can only choose rock paper or scissors
+// Make a randomizer to have the ai choose any element
+// Accept Player input
+  // Compare player input to computer chosen input
+  // Test win conditions
+  //Win cons are same element = tie
+  //Rock beats scissors
+  //Paper beats rock
+  //Scissors beat paper
+//Depending on win condition display “You win” or “Tie” or “You lose”
+
