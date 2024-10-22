0d701869616d69e44ba2fddde1f328efd11523d5
andreeamillan <anlmilla@ucsc.edu>
2024-10-21 21:50:04 -0700

    div

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -17,7 +17,7 @@ age = 2024 - year;
 
 document.writeln("Make: " + make + "<br>");
 document.writeln("Model: " + model + "<br>");
-document.writeln("Color: " + color + "<br");
+document.writeln("Color: " + color + "<br>");
 document.writeln("Year: " + year + "<br>");
 document.writeln("Age: " + age + " years<br>");
 document.writeln("Owned: " + ownIt + "<br>");
