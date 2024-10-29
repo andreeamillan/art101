2a005e5751acb5d30aa9bb7b3c7f23e40e27ecc4
andreeamillan <anlmilla@ucsc.edu>
2024-10-28 21:23:48 -0700

    JS

---
+++ lab7/js/lab.js
@@ -0,0 +1,23 @@
+/**
+ * Author: Andreea Millan
+ * Created: October 28, 2024
+ * Purpose: To sort / rearrange the user's name.
+ * 
+ */
+
+// sortUsername - function created to take user input through a browser/window prompt and rearrange the letters of their name.
+
+// declaring function and function variables
+function sortUsername() {
+    const userName = window.prompt("Hello! Tell me your name so I can fix it and make it better.")
+    console.log("userName =", userName); 
+
+
+//declaring other part of function, using code shortcut
+    var nameSorted = userName.toUpperCase().split("").sort().join("");
+    console.log("nameSorted =", nameSorted)
+    return nameSorted;
+}
+
+//output
+document.write("User's correct and better name is    " + sortUsername(), "</br>");
