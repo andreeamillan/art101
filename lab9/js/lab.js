ce51218bc42b75ec84e2625e60b7f23218e91a8d
andreeamillan <anlmilla@ucsc.edu>
2024-11-04 20:50:11 -0800

    css

---
+++ lab9/js/lab.js
@@ -0,0 +1,8 @@
+/*
+   lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Andreea Millan
+   Date: November 4, 2024
+*/
