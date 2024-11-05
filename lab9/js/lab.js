0c85efb5fc9621077f9367ae60ea6e2f4d41b1d8
andreeamillan <anlmilla@ucsc.edu>
2024-11-04 21:09:05 -0800

    JS

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -6,3 +6,36 @@
    Author: Andreea Millan
    Date: November 4, 2024
 */
+
+//Creating the buttons 
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+$("#problems").append("<button id='button-problems'>Make Special</button>");
+$("#results").append("<button id='button-results'>Make Special</button>");
+
+// add a click listener to the challenge button
+$("button-challenge").click(function(){
+    $("#challenge").toggleClass("special");
+});
+
+
+//
+$("button-Problems").click(function(){
+    $("#Problems").toggleClass("special");
+});
+
+//When special mode is off, initiates output
+$("#output").text("Special mode is Off");
+
+
+//
+$("#button-results").click(function(){
+    $("#results").toggleClass("special");
+
+    if($("results").hasClass("special")) {
+       $("#output").text("Special mode is On");
+    }
+    else {
+        $("#output").text("Special mode is Off");
+    }
+});
+
