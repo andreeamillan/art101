b215d67cf3e5268ae008790426e99213ec4b42e9
andreeamillan <anlmilla@ucsc.edu>
2024-11-06 14:29:02 -0800

    file updates in js and updated function

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -7,24 +7,31 @@
    Date: November 6, 2024
 */
 
-//function for generating fake dialogue
 function generateRandomText() {
     const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
     const min = 3;
-    const max = 100;
+    const max = 200;
     const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-// Random starting index to slice the text input
+    // Get a random starting index to slice the Lorem Ipsum text
     const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-//Generates random Lorem Ipsum text
-    return text.slice(randStart, randStart, randLen);
-}
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+  }
 
 //event listener for button
 $("#make-convo").click(function(){
+
+    //acquiring new fake dialogue
+    let newText = generateRandomText();
+    console.log("Click");
+
+    $("#output").append('<div class="text"><o>' + newText + '</p></div');
 });
 
-//acquiring new fake dialogue
-    const newText = generateRandomText();
-
+// click listener for submit button
+$("#submitButton").click(function(){
+    //userInput value
+    var input = $("#userInput").val();
 //appending new div to our output div
-$("#output").append('<div class="text"><p>' + newText + '</p></div>');
+    $("#output").append('<div class="text"><p>' + input + '</p></div>');
+});
