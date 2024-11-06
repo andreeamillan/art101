355f09a4ec4df55e9bfae59a5fc96586bfb9f0ca
andreeamillan <anlmilla@ucsc.edu>
2024-11-06 14:09:51 -0800

    convo

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -12,9 +12,10 @@ function generateRandomText() {
     const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
     const min = 3;
     const max = 100;
-    const randLen = Math.floor(Math.random()  * (max - min + 1)) + min;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
 // Random starting index to slice the text input
     const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+//Generates random Lorem Ipsum text
     return text.slice(randStart, randStart, randLen);
 }
 
@@ -26,4 +27,4 @@ $("#make-convo").click(function(){
 const newText = generateRandomText();
 
 //appending new div to our output div
-$("#output").append('<div class ="text"><p>' + newText + '</p></div>');
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
