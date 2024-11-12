5e6309f41de5c4daa8bb4c8b01f6204a931470a9
andreeamillan <anlmilla@ucsc.edu>
2024-11-11 21:09:42 -0800

    Lab11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -9,44 +9,37 @@
 
 
 // Generates an anagram of a given string.
-// credit to ChatGPT
+// credit to ChatGPT /Wmodes website 
 function anagram(inputString) {
-    // Convert the string to an array of characters
-    const charArray = inputString.split('');
+    // String to convert/split characters
+    const charArrange = inputString.split('');
    
-    // Use the Fisher-Yates (Knuth) shuffle algorithm to shuffle the characters
-    for (let i = charArray.length - 1; i > 0; i--) {
+    // Used to shuffle characters/letters (Ref to Wmodes website)
+    for (let i = charArrange.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [charArray[i], charArray[j]] = [charArray[j], charArray[i]];
+      [charArrange[i], charArrange[j]] = [charArrange[j], charArrange[i]];
     }
    
-    // Join the shuffled characters back into a string
-    const anagram = charArray.join('');
-   
-    // Return the generated anagram
+    // Joining shuffled values back into the string and returning the anagram.
+    const anagram = charArrange.join('');
+
     return anagram;
    }
    
    // given a string, return string in Title Case
-   // credit to ChatGPT
+   // credit to ChatGPT/Wmodes website
    String.prototype.toTitleCase = function() {
      return this.replace(/\w\S*/g, function(txt) {
        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
      });
    };
    
-   // Sorts the characters of a string in alphabetical order.
-   function sortString(inputString) {
-       // We have to convert our string to an array and back again to sort it
-       return inputString.split('').sort().join('');
-   }
-   
    // click listener for button
    $("#submit").click(function(){
-       // get value of input field
        const userName = $("#user-name").val();
-       // now let's sort it
-       newName = anagram(userName).toTitleCase();
-       // append a new div to our output div
+       const newName = anagram(userName).toTitleCase();
+    
+
+       // Appending new div to output
        $("#output").html('<div class="name-results">' + newName + '</div>');
    });
