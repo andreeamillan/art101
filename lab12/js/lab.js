b232de15fd727c77eeec0332e384df64060ed9c2
andreeamillan <anlmilla@ucsc.edu>
2024-11-14 15:34:46 -0800

    Js

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,45 +1,47 @@
 /*
-   lab.js - This simple JavaScript/jQuery script gets a value from an input field and outputs a modified version.
+   lab.js - This  JavaScript/jQuery script creates a sorting hat based on user input and button implementation.
 
    Requirements: jQuery must be loaded for this script to work.
 
    Author: Andreea Millan
-   Date: November 10, 2024
+   Date: November 14, 2024
 */
 
 
-// Generates an anagram of a given string.
-// credit to ChatGPT /Wmodes website 
-function anagram(inputString) {
-    // String to convert/split characters
-    const charArrange = inputString.split('');
-   
-    // Used to shuffle characters/letters (Ref to Wmodes website)
-    for (let i = charArrange.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [charArrange[i], charArrange[j]] = [charArrange[j], charArrange[i]];
-    }
-   
-    // Joining shuffled values back into the string and returning the anagram.
-    const anagram = charArrange.join('');
+// Return Gryffindor, Ravenclaw, Slytherin, Hufflepugg
+// depends on length mod 4
+function sortingHat(str)  {
+  len = str.length;
+  mod = len % 4;
+  if (mod ==0)  {
+    return "Gryffindor"
+  }
+  else if (mod==1)  {
+    return "Ravenclaw"
+  }
+  else if (mod==2)  {
+    return "Slytherin"
+  }
+  else if (mod==3)  {
+    return "Hufflepuff"
+  }
+}
 
-    return anagram;
-   }
-   
-   // given a string, return string in Title Case
-   // credit to ChatGPT/Wmodes website
-   String.prototype.toTitleCase = function() {
-     return this.replace(/\w\S*/g, function(txt) {
-       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-     });
-   };
-   
-   // click listener for button
-   $("#submit").click(function(){
-       const userName = $("#user-name").val();
-       const newName = anagram(userName).toTitleCase();
-    
+//creating button / click listener 
+$("#button").click(function(){
+  var name = $("#input").val()
+  console.log(name);
+  var nameLength = name.length;
+  console.log(nameLength);
+  var house = getHouse(nameLength);
+  console.log(house);
 
-       // Appending new div to output
-       $("#output").html('<div class="name-results">' + newName + '</div>');
-   });
+  let newText = sortingHat();
+  newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+
+  $("#output").html("<h1>" + house + "</h1>");
+});
+
+
+  
+
