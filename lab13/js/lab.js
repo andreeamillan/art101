a326b3d0035a04960c4f7cb674539550562a45e7
andreeamillan <anlmilla@ucsc.edu>
2024-11-18 16:16:33 -0800

    lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,42 +1,12 @@
 /*
-   lab.js - This  JavaScript/jQuery script creates a sorting hat based on user input and button implementation.
+   lab.js - This  JavaScript/jQuery script creates a loop function that prints different words.
 
    Requirements: jQuery must be loaded for this script to work.
 
    Author: Andreea Millan
-   Date: November 14, 2024
+   Date: November 18, 2024
 */
 
-
-// Return Gryffindor, Ravenclaw, Slytherin, Hufflepugg
-// depends on length mod 4
-function sortingHat(str)  {
-  len = str.length;
-  mod = len % 4;
-  if (mod ==0)  {
-    return "Gryffindor"
-  }
-  else if (mod==1)  {
-    return "Ravenclaw"
-  }
-  else if (mod==2)  {
-    return "Slytherin"
-  }
-  else if (mod==3)  {
-    return "Hufflepuff"
-  }
-}
-
-//creating button / click listener 
-$("#button").click(function(){
-  let name = $("#input").val();
-  console.log(name);
-
-  let house = sortingHat(name);
-  console.log(house);
-
-  $("#output").html("<h1>" + house + "</h1>");
-});
 
 
   
