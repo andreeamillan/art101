a5d55c891e87479ec9cae2d178b76ca8bd6ff0fd
andreeamillan <anlmilla@ucsc.edu>
2024-11-20 21:38:49 -0800

    lab

---
+++ lab14/js/lab.js
@@ -0,0 +1,14 @@
+/*
+   lab.js - This  JavaScript/jQuery script creates a sorting hat based on user input and button implementation.
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Andreea Millan
+   Date: November 14, 2024
+*/
+
+
+
+
+  
+
--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -9,15 +9,17 @@
 
 // declaring function and function variables
 function sortUsername() {
-    const userName = window.prompt("Hello! Tell me your name so I can fix it and make it better.")
+    const userName = window.prompt("Hello! Tell me your name so I can fix it and make it better.");
     console.log("userName =", userName); 
 
-
-//declaring other part of function, using code shortcut
+    // declaring other part of function, using code shortcut
     var nameSorted = userName.toUpperCase().split("").sort().join("");
-    console.log("nameSorted =", nameSorted)
+    console.log("nameSorted =", nameSorted);
     return nameSorted;
 }
 
-//output
-document.write("User's correct and better name is    " + sortUsername(), "</br>");
+// Only call the function once and store the result
+const sortedName = sortUsername();
+
+// Output the result in the document
+document.write("User's correct and better name is: " + sortedName + "</br>");
