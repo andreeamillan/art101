cd86ab4695ee2e05ea3c71356d3bd2ba94d31c94
andreeamillan <anlmilla@ucsc.edu>
2024-12-03 17:53:59 -0800

    output

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -6,33 +6,48 @@
 */
 
 // Using the core $.ajax() method
-$.ajax({
-   // The URL for the request (from the api docs)
-   url: "https://yourapiendpoint.com/",
-   // The data to send (will be converted to a query string)
-   data: { 
-           // here is where any data required by the api 
-           //   goes (check the api docs)
-           id: 123,
-           api_key: "blahblahblah",
-         },
-   // Whether this is a POST or GET request
-   type: "GET",
-   // The type of data we expect back
-   dataType : "json",
-   // What do we do when the api call is successful
-   //   all the action goes in here
-   success: function(data) {
-       // do stuff
-       console.log(data);
-   },
-   // What we do if the api call fails
-   error: function (jqXHR, textStatus, errorThrown) { 
-       // do stuff
-       console.log("Error:", textStatus, errorThrown);
-   }
-})
+$(document).ready(function() {
+  // Send an AJAX request to retrieve the comic data from the XKCD API
+  $.ajax({
+    url: 'https://xkcd.com/899/info.0.json',  // XKCD API URL
+    method: 'GET',
+    success: function(comicObj) {
+      console.log('AJAX request successful!');
+      console.log('Received comic data:', comicObj);
 
+      // Ensure that the comicObj has the necessary keys before proceeding
+      if (comicObj && comicObj.title && comicObj.img && comicObj.alt) {
+        // Create a section element for the comic
+        var comicSection = $('<section></section>').addClass('comic-section');
+
+        // Set the section title from the comicObj.title
+        var comicTitle = $('<h2></h2>').text(comicObj.title);
+        comicSection.append(comicTitle);
+
+        // Create an image tag using the comicObj.img and set alt and title attributes
+        var comicImage = $('<img>');
+        comicImage.attr('src', comicObj.img)       // The URL of the comic image
+                  .attr('alt', comicObj.alt)     // Alt text for the image (accessible)
+                  .attr('title', comicObj.alt);  // Title text for the image (shown on hover)
+
+        // Append the image to the section
+        comicSection.append(comicImage);
+
+        // Append the section to the #comic-container div
+        $('#comic-container').html(comicSection);
+      } else {
+        // Handle case where necessary data is missing in comicObj
+        console.error('Comic data is missing required fields!');
+        $('#comic-container').html('<p>Failed to load valid comic data. Some required fields are missing.</p>');
+      }
+    },
+    error: function(xhr, status, error) {
+      // Handle errors if the AJAX request fails
+      console.error('AJAX request failed:', status, error);
+      $('#comic-container').html('<p>Failed to load comic data. Please try again later.</p>');
+    }
+  });
+});
 
   
 
